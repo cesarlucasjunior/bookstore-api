c7277315f23bfe23063bbb8be68c25da6cb53d18
César Lucas Júnior <cesarlucasjunior93@gmail.com>
2025-09-11 16:38:01 -0300

    Cria servico responsavel por salvar objeto na bucket e retorna string

---
+++ src/main/java/br/com/cesarlucasjunior/bookstore_api/service/S3Service.java
@@ -0,0 +1,40 @@
+package br.com.cesarlucasjunior.bookstore_api.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+
+@Service
+public class S3Service {
+
+    private final S3Client s3Client;
+    private final String bucket = "bookstore-bucket";
+
+    public S3Service(S3Client s3Client) {
+        this.s3Client = s3Client;
+    }
+
+    public String uploadFile(MultipartFile file) throws IOException {
+        String key = UUID.randomUUID() + "_" + file.getOriginalFilename();
+
+        s3Client.putObject(
+                PutObjectRequest.builder()
+                        .bucket(bucket)
+                        .key(key)
+                        .contentType(file.getContentType())
+                        .build(),
+                RequestBody.fromBytes(file.getBytes())
+        );
+
+        return String.format("http://localhost:4566/%s/%s",
+                bucket,
+                URLEncoder.encode(key, StandardCharsets.UTF_8));
+    }
+}
